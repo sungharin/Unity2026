d4a0b31654b06403073d5e5986e4880028b8c574
sungharin <sungharin05@naver.com>
2026-03-30 10:54:14 +0900

    ch04(26.3.30)

---
+++ ch04/Assets/2.Scripts/CarController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    float speed = 0f;
+    Vector2 startPose;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Application.targetFrameRate = 60;  
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            startPose = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 endpos = Input.mousePosition;
+            float swipeLen = endpos.x - startPose.x;
+            speed =  swipeLen / 500f;
+        }
+        transform.Translate(speed,0,0);
+        speed *= 0.99f;
+    }
+}
